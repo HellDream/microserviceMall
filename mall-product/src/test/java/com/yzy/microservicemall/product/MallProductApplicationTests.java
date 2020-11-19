1c36d6d88f08a419a96854a0bf4902bf9f325a09
Zhenyu Ye <yzy673598755@gmail.com>
2020-11-18 17:02:04 -0800

    project initialized

---
+++ mall-product/src/test/java/com/yzy/microservicemall/product/MallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzy.microservicemall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
